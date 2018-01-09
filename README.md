4145514eef5c6dad36efec75987f9cc530448405
Matt Amacker <matt.amacker@Matt-Amackers-MacBook-Pro-15.local>
2018-01-09 15:30:39 -0800

    Lots of little pycom devices.

--- README.md
+++ README.md
@@ -5,7 +5,10 @@ First esp8266 code that doesn't crash the processor.
  * Follow steps here:
   https://learn.adafruit.com/micropython-basics-how-to-load-micropython-on-a-board/esp8266
  * Which will look like this in the end:
+ ** ESP8266
  sudo esptool.py --port /dev/tty.SLAB_USBtoUART  write_flash --flash_size=detect 0 /Users/matt.amacker/Downloads/esp8266-20171101-v1.9.3.bin
+ ** ESP32
+ sudo esptool.py --chip esp32 --port /dev/tty.SLAB_USBtoUART  write_flash -z 0x1000 /Users/matt.amacker/Downloads/esp32-20180108-v1.9.3-238-g42c4dd09.bin
  * The follow steps to install ampy:
   https://learn.adafruit.com/micropython-basics-load-files-and-run-code/install-ampy
 
---
+++ glass/main.py
@@ -0,0 +1,53 @@
+from machine import Pin
+import utime
+import gc
+from urequests import get
+
+from machine import I2C
+i2c = I2C(sda=Pin(23), scl=Pin(22), freq=400000)
+
+from vl6180 import Sensor
+
+print('Hello world! I can count to 10:')
+for i in range(1,11):
+    print(i)
+
+# create an output pin on pin #0
+print('Setup the pin...')
+pin = Pin(12, Pin.OUT)
+
+def do_connect():
+    import network
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    if not wlan.isconnected():
+        print('connecting to network...')
+        wlan.connect('ACME', 'roadrunner')
+        while not wlan.isconnected():
+            pass
+    print('network config:', wlan.ifconfig())
+
+def retrieve_url(url):
+    #gc.collect()
+    resp = None
+    try:
+        resp = get(url)
+        value = resp.text
+    except Exception as e: # Here it catches any error.
+        if isinstance(e, OSError) and resp: # If the error is an OSError the socket has to be closed.
+            resp.close()
+        value = {"error": e}
+    #gc.collect()
+    return value
+
+do_connect()
+print('Network is setup.')
+
+dist = Sensor(i2c);
+dist.init()
+print("Identifying.");
+print(dist.identify())
+
+
+while True:
+    res = retrieve_url('http://192.168.2.38:3000/' + str(dist.range() - 15));

b35d03e7f2941ea945920127bb6ebb7a2bf34c06
Matt Amacker <matt.amacker@ip-10-110-20-174.ec2.internal>
2018-02-21 17:40:05 -0800

    Works... just slowly.

--- throwy/main.py
+++ throwy/main.py
@@ -1,11 +1,15 @@
 from machine import Pin
 import utime
+import ustruct
+import time
+import usocket
+
 import gc
 from urequests import get
 from machine import I2C
 i2c = I2C(sda=Pin(23), scl=Pin(22), freq=400000)
 
-from vl6180 import Sensor
+from vl53l0x import VL53L0X
 
 print('Hello world! I can count to 10:')
 for i in range(1,11):
@@ -26,6 +30,10 @@ def do_connect():
             pass
     print('network config:', wlan.ifconfig())
 
+def select_channel(channel):
+    i2c.writeto(0x74, ustruct.pack('>B', 1 << channel ))
+    print ("Selected: ", channel, ustruct.pack('>B', 1 << channel ))
+
 def retrieve_url(url):
     #gc.collect()
     resp = None
@@ -42,15 +50,27 @@ def retrieve_url(url):
 do_connect()
 print('Network is setup.')
 
-dist = Sensor(i2c);
-dist.init()
-print("Identifying.");
-print(dist.identify())
+for i in range(4,7):
+    select_channel(i);
+    dist = VL53L0X(i2c);
+    dist.measurement_timing_budget = 20000;
+    #dist.io_timeout_s = 3000;
+
+    print("Identifying.");
+    print("Range: ", dist.range)
+#addr = usocket.getaddrinfo("192.168.2.204", 5005)[0][-1];
+addr = usocket.getaddrinfo("192.168.2.38", 3001)[0][-1];
 
 times = 0
 while True:
-    if times % 50 == 0:
-        dist.init()
-        if times > 10000000:
-            times = 0
-    res = retrieve_url('http://192.168.2.2:9080/add?source=throwy1&stat=vl6180&type=sample&value=' + str(dist.range() - 15));
+    select_channel(times % 3 + 4);
+    dist.start_range();
+    r = dist.range
+    print ("Range");
+    print (r);
+    times = times + 1
+
+    #s = usocket.socket(usocket.AF_INET, usocket.SOCK_DGRAM)
+    #s.sendto(ustruct.pack('>H',r), addr);
+    #s.close()
+    #res = retrieve_url('http://192.168.2.38:3000/add?source=throwy1&stat=vl6180&type=sample&value=' + str(dist.range() - 15));
--- throwy/vl6180.py
+++ throwy/vl6180.py
@@ -1,91 +1,72 @@
 import ustruct
 import time
 
+# VCNL4040 constants:
+VCNL4040_ALS_CONF            = 0x00
+VCNL4040_PS_CONF1            = 0x03 # NOTE: this also covers PSCONF2 reg
+VCNL4040_PS_CONF3            = 0x04 # NOTE: this also covers PSMS reg
+VCNL4040_PS_DATA             = 0x08
+VCNL4040_PS_DATAL             = 0x0008
+VCNL4040_ALS_DATA            = 0x09
+VCNL4040_INT_FLAG            = 0x0B # NOTE: this is the high byte
+VCNL4040_DEVICE_REG          = 0x0C
+VCNL4040_DEVICE_ID           = 0x186 # Value expected in the DEVICE_REG
+
 
 class Sensor:
     def __init__(self, i2c, address=0x29):
         self.i2c = i2c
         self._address = address
         self.init()
-        self.default_settings()
 
     def _set_reg8(self, address, value):
         data = ustruct.pack('>HB', address, value)
-        self.i2c.writeto(self._address, data)
+        try:
+            self.i2c.writeto(self._address, data)
+        except:
+            print("error set reg8");
+            None;
 
     def _set_reg16(self, address, value):
         data = ustruct.pack('>HH', address, value)
-        self.i2c.writeto(self._address, data)
+        try:
+            self.i2c.writeto(self._address, data)
+        except:
+            print("error set reg16");
 
     def _get_reg8(self, address):
         self.i2c.start()
         self.i2c.write(ustruct.pack('>BH', self._address << 1, address))
-        data = self.i2c.readfrom(self._address, 1)
-        return data[0]
+        time.sleep(.001);
+        data = [];
+        try:
+            data = self.i2c.readfrom(self._address, 1)
+            return data[0]
+        except:
+            print("error get reg8");
+        return 0
 
     def _get_reg16(self, address):
         self.i2c.start()
         self.i2c.write(ustruct.pack('>BH', self._address << 1, address))
-        data = self.i2c.readfrom(self._address, 2)
-        return ustruct.unpack('>B', data)[0]
+        time.sleep(.001);
+        data = [];
+        try:
+            data = self.i2c.readfrom(self._address, 2)
+            return ustruct.unpack('>B', data)[0]
+        except:
+            print("error get_reg16");
+        return 0
 
     def init(self):
-        if self._get_reg8(0x0016) != 1:
-            raise RuntimeError("Failure reset")
+        # Now that we know we are looking at our device, setup the config.
+        self._set_reg16(VCNL4040_ALS_CONF, 0x8000) # ALS enabled, no interrupts
+        self._set_reg16(VCNL4040_PS_CONF1, 0b0000111000000000) # high accuracy, and clear the shut down
+        self._set_reg16(VCNL4040_PS_CONF3, 0x0005) # no 'smart' modes
 
-        # Recommended setup from the datasheet
-        self._set_reg8(0x0207, 0x01)
-        self._set_reg8(0x0208, 0x01)
-        self._set_reg8(0x0096, 0x00)
-        self._set_reg8(0x0097, 0xfd)
-        self._set_reg8(0x00e3, 0x00)
-        self._set_reg8(0x00e4, 0x04)
-        self._set_reg8(0x00e5, 0x02)
-        self._set_reg8(0x00e6, 0x01)
-        self._set_reg8(0x00e7, 0x03)
-        self._set_reg8(0x00f5, 0x02)
-        self._set_reg8(0x00d9, 0x05)
-        self._set_reg8(0x00db, 0xce)
-        self._set_reg8(0x00dc, 0x03)
-        self._set_reg8(0x00dd, 0xf8)
-        self._set_reg8(0x009f, 0x00)
-        self._set_reg8(0x00a3, 0x3c)
-        self._set_reg8(0x00b7, 0x00)
-        self._set_reg8(0x00bb, 0x3c)
-        self._set_reg8(0x00b2, 0x09)
-        self._set_reg8(0x00ca, 0x09)
-        self._set_reg8(0x0198, 0x01)
-        self._set_reg8(0x01b0, 0x17)
-        self._set_reg8(0x01ad, 0x00)
-        self._set_reg8(0x00ff, 0x05)
-        self._set_reg8(0x0100, 0x05)
-        self._set_reg8(0x0199, 0x05)
-        self._set_reg8(0x01a6, 0x1b)
-        self._set_reg8(0x01ac, 0x3e)
-        self._set_reg8(0x01a7, 0x1f)
-        self._set_reg8(0x0030, 0x00)
-
-    def default_settings(self):
-        self._set_reg8(0x010A, 0x30) # Set Avg sample period
-        self._set_reg8(0x003f, 0x46) # Set the ALS gain
-        self._set_reg8(0x0031, 0xFF) # Set auto calibration period
-                                     # (Max = 255)/(OFF = 0)
-        self._set_reg8(0x0040, 0x63) # Set ALS integration time to 100ms
-        self._set_reg8(0x002E, 0x01) # perform a single temperature calibration
-
-        # Optional settings from datasheet
-        self._set_reg8(0x001B, 0x09) # Set default ranging inter-measurement
-                                     # period to 100ms
-        self._set_reg8(0x003E, 0x0A) # Set default ALS inter-measurement
-                                     # period to 100ms
-
-        # Additional settings defaults from community
-        self._set_reg8(0x001C, 0x32) # Max convergence time
-        self._set_reg8(0x002D, 0x10 | 0x01) # Range check enables
-        self._set_reg8(0x0022, 0x7B) # Eraly coinvergence estimate
-        self._set_reg8(0x0120, 0x01) # Firmware result scaler
 
     def identify(self):
+        print("Identify: ", self._get_reg16(VCNL4040_DEVICE_REG));
         """Retrieve identification information of the sensor."""
         return {
             'model': self._get_reg8(0x0000),
@@ -96,17 +77,12 @@ class Sensor:
             'time': self._get_reg16(0x008),
         }
 
-    def address(self, address=None):
-        """Change the I2C address of the sensor."""
-        if address is None:
-            return self._address
-        if not 8 <= address <= 127:
-            raise ValueError("Wrong address")
-        self._set_reg8(0x0212, address)
-        self._address = address
+    def dump_register_range(self, start, end):
+        print ("Printing registers 0x%x -> 0x%x" % (start,end))
+        for reg in range(start,end+1):
+            val = self._get_reg16(reg)
+            print ("[0x%x] 0x%x" % (reg, val))
 
     def range(self):
         """Measure the distance in millimeters. Takes 0.01s."""
-        self._set_reg8(0x0018, 0x01) # Sysrange start
-        time.sleep(0.01)
-        return self._get_reg8(0x0062) # Result range value
+        return self._get_reg16(VCNL4040_PS_DATAL) # Result range value

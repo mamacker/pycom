f349d87dc4484aad7ec678371e10f54f992915f1
Matt Amacker <matt.amacker@ip-10-110-120-85.ec2.internal>
2018-06-21 15:53:59 -0700

    simplification

--- super/main.py
+++ super/main.py
@@ -9,12 +9,12 @@ pinInput = machine.Pin(22, machine.Pin.IN, machine.Pin.PULL_UP)
 
 np = neopixel.NeoPixel(np_pin, 32, timing=True)
 
-val = "family0"
-val_color = (255,0,0)
+#val = "family0"
+#val_color = (255,0,0)
 #val = "family1"
 #val_color = (125,125,125)
-#val = "family2"
-#val_color = (0,0,255)
+val = "family2"
+val_color = (0,0,255)
 
 for i in range(np.n):
     np[i] = val_color
@@ -61,13 +61,14 @@ caller_pinged = False
 
 last_server_val = 0;
 while True:
+    ''' 
     if utime.ticks_ms() - last_server_check > 5000:
         # Check the button state on the server.
         print ("Calling the server for get");
         res = retrieve_url('http://10.110.120.85:3000/button/get?which=' + str(val))
 
         print ("Result: " + str(res));
-        try: 
+        try:
             if int(res + "") > last_server_val:
                 print ("Button set... starting blink.");
                 if not caller_pinged:
@@ -94,18 +95,21 @@ while True:
         last_blink = utime.ticks_ms()
         blink_on = not blink_on
 
+    '''
     # Watch the pin to see if we need to set our
     # state on the server
     if pinInput.value() == 0:
+        '''
         if caller_pinged:
             print("Setting button CLEARED with server...");
             retrieve_url('http://10.110.120.85:3000/button/clear?which=' + str(val))
             caller_pinged = False
             clear_lights();
         else:
-            print("Setting button on with server...");
-            blink_on = True
-            last_blink = utime.ticks_ms()
-            retrieve_url('http://10.110.120.85:3000/button/set?which=' + str(val))
-            caller_pinged = True
+        '''
+        print("Setting button on with server...");
+        blink_on = True
+        last_blink = utime.ticks_ms()
+        retrieve_url('http://10.110.120.85:3000/button/set?which=' + str(val))
+        caller_pinged = True
 
